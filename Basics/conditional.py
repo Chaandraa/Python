6b10de7ef54402e8f91c57085f27846175b8d3f5
Chaandraa <piku26102003@gmail.com>
2026-06-29 15:46:42 +0530

    Added conditionals file

--- Basics/conditional.py
+++ Basics/conditional.py
@@ -1,76 +1,76 @@
-"""
-conditional statements in python
-
-you cab use logical operators as well.
-    """
-    
-# Q1. Accept 2 numbers from user and print the greatest number among them.
-"""
-a = float(input("Enter first number: "))
-
-b = float(input("Enter second number: "))
- 
-
-if a > b:
-    print("The greatest number is:", a)
-elif b > a:
-    print("The greatest number is:", b)
-else:
-        print("Both numbers are equal.")
-"""
- 
- #Q2. Accept gender from user and print message accordingly.ex: if user enters "male" then print "Good morning Sir"
-"""gender = input("Enter your gender (male/female): ").lower()
-
-user_input = gender[0]
-
-if not user_input:
-    print("Please enter a your gender it is required")
-    
-if user_input == "m":
-    print("Good morning Sir")
-else:
-    print("Good morning Ma'am")            
-"""
-
-#Q3.Accept an integer from user and check whether it is even or odd.
-"""
-number = input("Enter an integer: ")
-try: #to handle the error if user enters a non-integer value
-    num= int(number)
-        
-    if number % 2 == 0:
-        print("The number is even.")
+"""num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+if num1.isdigit() and num2.isdigit():
+    num1=int(num1)
+    num2=int(num2)
+    if num1>num2:
+        print(f"{num1} is greater than {num2}")
+    elif num1<num2:
+        print(f"{num2} is greater than {num1}")
     else:
-        print("The number is odd.")
+        print(f"{num1} and {num2} are equal.")
+else:
+    print("Enter valid numbers only.")"""
+    
+"""gender = input("Please enter your gender: ")
 
-except ValueError:
-    print("Invalid input. Please enter an integer.") 
+if gender.lower() == "male":
+    print(" Good Morning! Sir.")
+elif gender.lower() == "female":
+    print("Good Morning! Ma'am.")
+else:
+    print("Invalid gender entered.")"""
+
+"""
+num = input("Enter a number: ")    
+
+if num.isdigit():
+    num = int(num)
+    if num%2 == 0:
+        print(f"{num} is an even number.")
+    else:
+        print(f"{num} is an odd number.")
+else:
+    print("Please enter a valid number.")
 """
 
-#Q4.accept name and age from user and find out whether the person is eligible to vote or not. (age should be greater than or equal to 18)
-#name = input("Enter your name:")
-#try:
-#    age = int(input("enter your age:"))
+"""name = input("Please enter your name: ")
+if name.isalpha():
+    print(f"Hello, {name}! Please enter your age to check if you are eligible to vote.")
+    age = input("Enter your age: ")
+    if age.isdigit():
+        age = int(age)
+        if age >= 18:
+            print(f"Congratulations, {name}! You are eligible to vote.")
+        else:
+            print(f"Sorry, {name}. You are not eligible to vote yet.")
+    else:
+        print("Please enter a valid age.")"""
 
-#    if age <0 or age >= 120:
-#        print("Please enter a valid , realistic age.")
-#    elif age >= 18:
-#        print(f"{name} is eligible to vote.")
-#    else:
-#        print(f"{name} is not eligible to vote.")    
-#except ValueError:
-#    print("Invalid input. Please enter a valid age.")        
 
-#Q5. Accept year from user and check if its a leap year or not. (A leap year is divisible by 4 but not by 100, except if it is divisible by 400)
-# year = input("Enter a year: ")
-# try:
-    # year = int(year)
-    # if(year % 4 == 0 or year % 400 == 0) and year % 100 != 0:
-        # print(f"{year} is a leap year.")
-    # else:
-        # print(f"{year} is not a leap year.")
-# except ValueError:
-    # print("Invalid input. Please enter a valid year.")
+"""year = int(input("Please enter a year: "))
 
-  
+if (year % 4==0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")"""
+    
+"""temp = input("Please enter the temperature in Celsius: ")
+temp = float(temp)
+if temp < 0:
+        print("Freezing")  
+    
+elif 0<= temp <10:
+        print("Very Cold")
+elif 10<= temp <20:
+        print("Cold")  
+elif 20<= temp <30:
+        print("Pleasant")
+elif 30<= temp <40:
+        print("Hot")
+else:
+        print("Very Hot")"""
+        
+        
+        
+                                                   
